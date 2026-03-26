e961cec8302ee4e0bcccd945cd4570d5d69755b1
dr0038-design <dr0038@srmist.edu.in>
2026-03-26 14:00:38 +0530

    Coded UC2: Display Hello with Command-Line Argument

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,7 +1,14 @@
 public class HelloApp {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World");
-
-    }
+        public static void main(String[] args)
+        {
+                // check if name is given
+                if(args.length > 0)
+                {
+                        System.out.println("Hello " + args[0]);
+                }
+                else
+                {
+                        System.out.println("Hello User");
+                }
+        }
 }
